4193f78aae78876e4eea977323e005caac9567c4
Sitong <nickren93@gmail.com>
2025-03-10 22:30:52 -0400

    complete code-along

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,28 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    /*
+    //Arrage:
     const birthYear = 1984;
+
+    //Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    //Assert
     expect(ageOfPerson).toBe(38);
+    */
+
+    //below are updated codes:
+    //Arrage
+    const currentYear = new Date().getFullYear();
+    const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    // Act
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    // Assert
+    expect(ageOfPerson).toEqual(answer);
+
   });
 });
